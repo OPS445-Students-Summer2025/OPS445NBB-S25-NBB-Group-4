3fe04f09d4b36d2d58edc19b8512a25b15e85d10
Raffy Limon <rnlimon@myseneca.ca>
2025-07-29 16:51:42 -0400

    Initial template: Set up function blocks and team member tasks

---
+++ assignment2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+'''
+OPS445 Assignment 2
+Program: assignment2.py 
+Topic: Memory usage tracker - A script that scans the host computer and returns total memory and used memory. 
+MVP: 1. Overall memory usage 2. By application name -> process id
+Additonal: Lists per process memory use, sorted by percent
+Author: Group 4
+Members: Aung Kaung Satt, Daniel Rhodes, Marian Derlina Fernando, Raffy Limon, Yuefan Zhang
+'''
+
+import os
+
+# Raffy-Limon:read /proc/meminfo block
+def name_here():
+    #Assigned task: Raffy Limon to implement: read from /proc/meminfo
+    pass
+
+
+
+
+
+
+# Daniel Rhodes: gather per‑process memory usage block
+def name_here():
+    #Assigned task: Daniel Rhodes to implement: get process memory usage
+    pass
+
+
+
+
+
+# Aung Kaung Satt: memory usage display block 
+def name_here():
+    #Assigned task: Aung Kaung Satt to implement: calculate used memory and print header
+    
+
+
+# Yuefan Zhang: sorting and displaying top processes - put inside memory usage display block
+
+
+
+
+# Marian Derlina Fernando: Additional features: 
+# --showGB   show values in GiB instead of MiB
+# --loop N   refresh every N seconds until Ctrl‑C
+def name_here():
+#Assigned task: Marian Derlina Fernando to implement: handle command-line arguments
+    pass
+
+
+
+
+
+def main():
+    #Assigndd task: Marian Derlina Fernando to implement: main block
+    pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
